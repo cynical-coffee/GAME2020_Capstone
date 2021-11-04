976ec013655e6723dc5f19dbcf8fad126856a2b5
Meghan <mparis003@gmail.com>
2021-11-04 12:52:31 -0400

    Created project files.

---
+++ Source/GAME2020_Capstone/GAME2020_Capstone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAME2020_Capstone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAME2020_Capstone, "GAME2020_Capstone" );
---
+++ Source/GAME2020_Capstone/GAME2020_CapstoneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAME2020_CapstoneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME2020_CAPSTONE_API AGAME2020_CapstoneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
